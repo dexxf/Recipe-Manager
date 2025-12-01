73f139f0d580f85a883089dd655e8a2536ec995b
dexxf <Dexter101306@gmail.com>
2025-12-01 13:36:34 +0800

    refactor(auth): move SecurityConfig from adapter to infrastructure

--- src/main/java/com/io/dexxf/infrastructure/auth/config/SecurityConfig.java
+++ src/main/java/com/io/dexxf/infrastructure/auth/config/SecurityConfig.java
@@ -1,24 +1,17 @@
-package com.io.dexxf.adapter.auth.out.security;
+package com.io.dexxf.infrastructure.auth.config;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import com.io.dexxf.adapter.auth.out.security.CustomUserDetailsService;
+import com.io.dexxf.adapter.auth.out.security.UserPrincipal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
